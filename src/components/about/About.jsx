b9b20d85e12910530e006b5da55acf431a29873f
FrederickAndersonn <frederickanderson71@yahoo.com>
2024-05-02 22:34:30 +0200

    updated data

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -12,10 +12,10 @@ const About = () => {
         <div className="about__container grid">
             <div className="image__container"><img src={Aboutimg} alt="" className="about__img" width="200px" /></div>
             <div className="about__data grid">
-                <p className="about__description">I'm Frederick Anderson, a dedicated student currently in my 4th semester at Hochschule Darmstadt. My passion lies in the dynamic fields of software engineering and data analytics. With a relentless curiosity and a desire to explore the ever-evolving tech landscape, I'm committed to learning, growing, and pushing the boundaries of what's possible.
+                <p className="about__description">I'm Frederick Anderson, a dedicated student currently in my 5th semester at Hochschule Darmstadt. My passion lies in the dynamic fields of software engineering and data analytics. With a relentless curiosity and a desire to explore the ever-evolving tech landscape, I'm committed to learning, growing, and pushing the boundaries of what's possible.
 As a future software engineer and data analyst, I believe in the power of data-driven solutions and elegant code. I'm on a journey to make meaningful contributions to the world of technology. Join me as I navigate this exciting adventure, one code snippet at a time.
 Let's build a smarter, data-driven future together.</p>
-<p className="about__description__short">I'm Frederick Anderson, a dedicated student currently in my 4th semester at Hochschule Darmstadt. My passion lies in the dynamic fields of software engineering and data analytics.</p>
+<p className="about__description__short">I'm Frederick Anderson, a dedicated student currently in my 5th semester at Hochschule Darmstadt. My passion lies in the dynamic fields of software engineering and data analytics.</p>
             </div>
             <div className="about__skills grid">
                 {skills.map((skill)=>{
--- src/components/portfolio/Menu.jsx
+++ src/components/portfolio/Menu.jsx
@@ -1,29 +1,76 @@
-import Work1 from "../../assets/rmp.png";
-import Work2 from "../../assets/ivc.png";
-import Work3 from "../../assets/dg.png";
-
+import rmp from "../../assets/rmp.png";
+import ivc from "../../assets/ivc.png";
+import dg from "../../assets/dg.png";
+import gl from "../../assets/gl.png";
+import imagepredictor from "../../assets/predictor.png";
+import opencv from "../../assets/opencv.png";
+import pizza from "../../assets/pizza.png";
+import cheerio from "../../assets/cheerio.png";
+import weconnect from "../../assets/weconnect.png";
 
 const Menu = [
     {
       id: 1,
-      image: Work1,
+      image: rmp,
       title: "RateMyProfessor",
       category: "App",
       link: "https://github.com/FrederickAndersonn/RateMyProf"
     },
     {
       id: 2,
-      image: Work2,
+      image: ivc,
       title: "Intrinsic Value Calculator",
       category: "Web",
       link: "https://653e7536aa0d3409c7ebe53a--dulcet-kashata-cdc8f4.netlify.app"
     },
     {
       id: 3,
-      image: Work3,
+      image: dg,
       title: "Dungeon Crawler",
       category: "Game",
       link: "https://github.com/FrederickAndersonn/DungeonCrawler"
+    },
+    {
+      id: 4,
+      image: gl,
+      title: "Lighting and Shadow",
+      category: "Others",
+      link: "https://www.youtube.com/watch?v=u2BmIeGjimk"
+    },
+    {
+      id: 5, 
+      image: imagepredictor,
+      title: "Image Prediction",
+      category: "Data Analytics",
+      link: "https://github.com/FrederickAndersonn/ImagePredictor"
+    },
+    {
+      id: 6,
+      image: opencv,
+      title: "Color Transfer",
+      category: "Others",
+      link: "https://github.com/FrederickAndersonn/ColorTransfer"
+    },
+    {
+      id: 7,
+      image: pizza,
+      title: "Pizza Service",
+      category: "Web",
+      link: "https://github.com/FrederickAndersonn/PizzaService"
+    },
+    {
+      id: 8,
+      image: cheerio,
+      title: "Cheerio Travel Web",
+      category: "Web",
+      link: "https://github.com/FrederickAndersonn/CheerioTravelApp"
+    },
+    {
+      id: 9,
+      image: weconnect,
+      title: "We Connect Enterprises",
+      category: "Web",
+      link: "https://we-connect-enterprises.netlify.app"
     },
   ];
 
